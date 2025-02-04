b69e702dc17c24a01ac23b21e3ac24d29b5a9fa5
mfjaconis <matheusjaconis@gmail.com>
2025-02-04 13:58:09 -0300

    utilizando jquery para chamar uma requisição e exibir no input endereço, o endereço encontrado na busca

--- main.js
+++ main.js
@@ -1,12 +1,29 @@
-document.addEventListener('DOMContentLoaded', () => {
-    document.getElementById('btn-buscar-cep').addEventListener('click', () => {
-        //AJAX - Asynchronous JavaScript and XML
-        const xhttp = new XMLHttpRequest();
-        const cep = document.getElementById('cep').value;
+// document.addEventListener('DOMContentLoaded', () => {
+//     document.getElementById('btn-buscar-cep').addEventListener('click', () => {
+//         //AJAX - Asynchronous JavaScript and XML
+//         const xhttp = new XMLHttpRequest();
+//         const cep = document.getElementById('cep').value;
+//         const endpoint = `https://viacep.com.br/ws/${cep}/json`;
+
+//         xhttp.open('GET', endpoint);
+//         xhttp.send()
+    
+//     })
+// })
+
+$(document).ready(() => {
+    $('#btn-buscar-cep').click(() => { 
+        const cep = $('#cep').val();
         const endpoint = `https://viacep.com.br/ws/${cep}/json`;
 
-        xhttp.open('GET', endpoint);
-        xhttp.send()
-    
+        $.ajax(endpoint).done((resposta) => {
+            const logradouro = resposta.logradouro;
+            const bairro = resposta.bairro;
+            const cidade = resposta.localidade;
+            const estado = resposta.uf;
+            const endereco = `${logradouro}, ${bairro} - ${cidade} - ${estado}`
+
+            $('#endereco').val(endereco)
+        })
     })
 })
