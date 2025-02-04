ba75393d4b349e2ce23b7da0ecd09388bc70ecae
mfjaconis <matheusjaconis@gmail.com>
2025-02-04 13:46:22 -0300

    utilizando javascript puro para chamar um requisição

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('btn-buscar-cep').addEventListener('click', () => {
         //AJAX - Asynchronous JavaScript and XML
-        
+        const xhttp = new XMLHttpRequest();
+        const cep = document.getElementById('cep').value;
+        const endpoint = `https://viacep.com.br/ws/${cep}/json`;
+
+        xhttp.open('GET', endpoint);
+        xhttp.send()
+    
     })
 })
