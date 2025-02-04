04ddca566739b17ac95a5f2f31d6fe86cff0456f
mfjaconis <matheusjaconis@gmail.com>
2025-02-04 13:37:37 -0300

    ciração do formulário e seus ID'S para ser recuprados e manipulados pelo javascript

---
+++ main.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('btn-buscar-cep').addEventListener('click', () => {
+        //AJAX - Asynchronous JavaScript and XML
+        
+    })
+})
