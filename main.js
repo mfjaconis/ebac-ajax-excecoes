9e27327936a3294caec12f31f3e05b6aff6b5be3
mfjaconis <matheusjaconis@gmail.com>
2025-02-04 14:15:38 -0300

    adicionando estilo ao botão clicar e dinamica ao botão buscar com jquery e bootstrap

--- main.js
+++ main.js
@@ -15,6 +15,9 @@ $(document).ready(() => {
     $('#btn-buscar-cep').click(() => { 
         const cep = $('#cep').val();
         const endpoint = `https://viacep.com.br/ws/${cep}/json`;
+        const botão = $('#btn-buscar-cep');
+        botão.find('i').addClass('d-none');
+        botão.find('span').removeClass('d-none');
 
         $.ajax(endpoint).done((resposta) => {
             const logradouro = resposta.logradouro;
@@ -24,6 +27,11 @@ $(document).ready(() => {
             const endereco = `${logradouro}, ${bairro} - ${cidade} - ${estado}`
 
             $('#endereco').val(endereco)
+            
+            setTimeout(() => {
+                botão.find('i').removeClass('d-none');
+                botão.find('span').addClass('d-none');
+            }, 1000);
         })
     })
 })
